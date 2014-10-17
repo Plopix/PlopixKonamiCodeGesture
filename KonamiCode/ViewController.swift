822456d9b98a70ee6e943b354eaca9383f140e95
Morel Sébastien <morel.seb@gmail.com>
2014-10-17 01:06:55 -0700

    Initial Commit

---
+++ KonamiCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KonamiCode
+//
+//  Created by Sebastien Morel on 10/17/14.
+//  Copyright (c) 2014 Plopix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
