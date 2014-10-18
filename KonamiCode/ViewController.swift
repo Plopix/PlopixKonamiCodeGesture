807b98343d395196a432e12df0f25227306c30c6
Morel Sébastien <morel.seb@gmail.com>
2014-10-18 14:19:54 -0700

    First version stable, not really tested

--- KonamiCode/ViewController.swift
+++ KonamiCode/ViewController.swift
@@ -13,8 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let recognizer = PlopixKonamiGesture(target: self, action: "launchEasterEgg:")
+        view.addGestureRecognizer(recognizer)
+        
     }
 
+    
+    func launchEasterEgg(recognizer: UITapGestureRecognizer) {
+        if ( recognizer.state == .Ended ) {
+            println("tapped button")
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
---
+++ PlopixKonamiGesture.swift
@@ -0,0 +1,210 @@
+//
+//  PlopixKonamiGesture.swift
+//  KonamiCode
+//
+//  Created by Sebastien Morel aka Plopix on 10/17/14.
+//  Copyright (c) 2014 Plopix. All rights reserved.
+//
+
+import UIKit
+
+class PlopixKonamiGesture: UIGestureRecognizer {
+    
+    struct Direction {
+        var up: CGVector
+        var down: CGVector
+        var left: CGVector
+        var right: CGVector
+        var neutral: CGVector
+        init() {
+            neutral = CGVectorMake(  0,  0 )
+            up      = CGVectorMake(  0, -1 )
+            down    = CGVectorMake(  0,  1 )
+            left    = CGVectorMake( -1,  0 )
+            right   = CGVectorMake(  1,  0 )
+        }
+        func isUp( vector: CGVector ) -> Bool {
+            return vector == self.up
+        }
+        func isDown( vector: CGVector ) -> Bool {
+            return vector == self.down
+        }
+        func isRight( vector: CGVector ) -> Bool {
+            return vector == self.right
+        }
+        func isLeft( vector: CGVector ) -> Bool {
+            return vector == self.left
+        }
+        func isNeutral( vector: CGVector ) -> Bool {
+            return vector == self.neutral
+        }
+    }
+    
+    // The directions
+    private let direction : Direction
+    
+    private let swipeDistanceTolerance : CGFloat = 50.0
+    private let swipeMinDistance : CGFloat = 50.0
+    
+    // Store the Konami Code
+    private var konamiCode: [CGVector] = []
+    
+    // Store the user code
+    private var currentCode : [CGVector] = []
+    
+    // Store the starting point of the animation
+    private var startingPoint: CGPoint = CGPointZero
+    
+    // init the Konami Code
+    override init(target: AnyObject, action: Selector) {
+        self.direction = Direction()
+        self.konamiCode = [
+            direction.up,direction.up,
+            direction.down,direction.down,
+            direction.left,direction.right,
+            direction.left,direction.right,
+            direction.neutral,direction.neutral]
+        super.init(target: target, action: action)
+    }
+    
+    private func nextGesture() -> CGVector? {
+        var succeedGesture = currentCode.count
+        if ( succeedGesture == konamiCode.count) {
+            return nil;
+        }
+        return konamiCode[succeedGesture]
+    }
+    
+    private func isOnHisWay( point: CGPoint ) -> Bool {
+        var next = self.nextGesture()
+        if (( next)  == nil ) {
+            return true
+        }
+        if ( direction.isNeutral( next! ) ) {
+            return true;
+        }
+        var deltaX: CGFloat = point.x - startingPoint.x;
+        var deltaY: CGFloat = point.y - startingPoint.y;
+        
+        // check the deviation
+        if ( direction.isUp( next! ) || direction.isDown( next! ) ) {
+            // next move in on Y, so we check X
+            if ( abs( deltaX ) > self.swipeDistanceTolerance ) {
+                return false;
+            }
+        }
+        if ( direction.isLeft( next! ) || direction.isRight( next! ) ) {
+            // next move in on X, so we check Y
+            if ( abs( deltaY ) > self.swipeDistanceTolerance ) {
+                return false;
+            }
+        }
+        // check the direction
+        if ( direction.isUp( next! ) ) {
+            if ( deltaY < 0 ) {
+                return true;
+            }
+        }
+        if ( direction.isDown( next! ) ) {
+            if ( deltaY > 0 ) {
+                return true;
+            }
+        }
+        if ( direction.isLeft( next! ) ) {
+            if ( deltaX < 0 ) {
+                return true;
+            }
+        }
+        if ( direction.isRight( next! ) ) {
+            if ( deltaX > 0 ) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
+    private func hasReachMinDistance( point: CGPoint ) -> Bool {
+        var deltaX: CGFloat = point.x - startingPoint.x;
+        var deltaY: CGFloat = point.y - startingPoint.y;
+        var next = self.nextGesture()
+        if (( next)  == nil ) {
+            return true
+        }
+        
+        if ( direction.isUp( next! ) || direction.isDown( next! ) ) {
+            if ( abs( deltaY ) > self.swipeMinDistance ) {
+                return true
+            }
+        }
+        if ( direction.isLeft( next! ) || direction.isRight( next! ) ) {
+            if ( abs( deltaX ) > self.swipeMinDistance ) {
+                return true
+            }
+        }
+        if ( direction.isNeutral( next! ) ) {
+            return true;
+        }
+        return false
+    }
+    
+    override func canPreventGestureRecognizer(preventedGestureRecognizer: UIGestureRecognizer!) -> Bool {
+        return false;
+    }
+    
+    override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!)  {
+        if ( event.touchesForGestureRecognizer(self)?.count > 1 ) {
+            // give a direct failed when more than touches are detected
+            self.state = UIGestureRecognizerState.Failed;
+            return
+        }
+        let touch:UITouch = touches.anyObject() as UITouch
+        self.startingPoint = touch.locationInView(self.view)
+        if ( self.state == UIGestureRecognizerState.Changed ) {
+            // do nothing now
+            //@todo: add time check
+            return
+        }
+        if ( self.state == UIGestureRecognizerState.Possible ) {
+            // only the first time
+            self.state = UIGestureRecognizerState.Began;
+            return
+        }
+        self.state = UIGestureRecognizerState.Failed;
+    }
+    
+    override func touchesMoved(touches: NSSet!, withEvent event: UIEvent!) {
+        let touch:UITouch = touches.anyObject() as UITouch
+        if ( !self.isOnHisWay(touch.locationInView(self.view)) ) {
+            self.state = UIGestureRecognizerState.Failed;
+        }
+    }
+    
+    override func touchesEnded(touches: NSSet!, withEvent event: UIEvent!) {
+        let touch:UITouch = touches.anyObject() as UITouch
+        let endPoint:CGPoint = touch.locationInView(self.view)
+        
+        if ( self.isOnHisWay(endPoint) && self.hasReachMinDistance(endPoint)  ) {
+            //go next or finish
+            var next = self.nextGesture()
+            if (( next)  != nil ) {
+                self.currentCode.append(next!)
+            }
+            if ( self.currentCode == self.konamiCode ) {
+                self.state = UIGestureRecognizerState.Ended;
+            }
+            return
+        }
+        self.state = UIGestureRecognizerState.Failed;
+    }
+    
+    override func touchesCancelled(touches: NSSet!, withEvent event: UIEvent!) {
+        self.reset()
+    }
+    
+    override func reset() {
+        currentCode = []
+        self.startingPoint = CGPointZero
+        super.reset()
+    }
+}
